da7541dd505ca1c61ef158098effef80a2313478
coffeecat123 <coffeeccaatt@gmail.com>
2022-06-12 13:38:46 +0800

    Initial commit

--- chrome extensions/cft_txt/background.js
+++ chrome extensions/cft_txt/background.js
@@ -8,14 +8,16 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.local.get(['trash','data','dark'], function(result) {
     if(result.data!=undefined)data=result.data;
     if(result.dark!=undefined)dark=result.dark;
+    if(result.htcr!=undefined)htcr=result.htcr;
     if(result.durl!=undefined)durl=result.durl;
     if(result.trash!=undefined)trash=result.trash;
-    chrome.storage.local.set({trash,data,dark});
+    chrome.storage.local.set({htcr,trash,durl,data,dark});
   });
 });
 chrome.storage.onChanged.addListener(function(changes, namespace) {
   if(changes.data!=undefined)data=changes.data.newValue;
   if(changes.dark!=undefined)dark=changes.dark.newValue;
   if(changes.durl!=undefined)durl=changes.durl.newValue;
+  if(changes.htcr!=undefined)htcr=changes.htcr.newValue;
   if(changes.trash!=undefined)trash=changes.trash.newValue;
 });
