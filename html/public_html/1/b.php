6237560335bc17946895e368b379c3ebd33599e0
coffeecat123 <coffeeccaatt@gmail.com>
2022-06-26 08:42:20 +0800

    2022/6/26

--- html/public_html/1/b.php
+++ html/public_html/1/b.php
@@ -1,25 +1,26 @@
 <?php
+$ip=str_replace(":",".",$_SERVER['REMOTE_ADDR']);
 echo "<style>body{background-color:rgb(200,200,200);}</style>";
 $file_path = "3.txt";
 $str=file_get_contents($file_path);
 fclose($file_path);
 $qa=0;
-if(preg_match("/{$_SERVER['REMOTE_ADDR']}/i",$str)){
+if(preg_match("/{$ip}/i",$str)){
     $qa=1;
 }
-//echo "IP: {$_SERVER['REMOTE_ADDR']}<br>";
+//echo "IP: {$ip}<br>";
 echo "<h1>Your Name: <i style='color:blue;'>".$_GET['yname']."</i></h1>";
 $file=fopen("1.txt","r");
 $n=fgets($file);
 fclose($file);
 $file=fopen("2.txt","a");
 fwrite($file,$n." ");
-fwrite($file,$_SERVER['REMOTE_ADDR']." ");
+fwrite($file,$ip." ");
 fwrite($file,$_GET['yname']." ");
 fwrite($file,$_GET['yng']."\n");
 fclose($file);
 //要建立的兩個檔案
-$qwe= "123/".$_SERVER['REMOTE_ADDR'].".txt";
+$qwe= "123/".$ip.".txt";
 //以讀寫方式打寫指定檔案，如果檔案不存則建立
 echo "這次提交的內容:".$_GET['yng']."<br>";
 if( ($TxtRes=fopen ($qwe,"a")) === FALSE){
@@ -29,7 +30,7 @@ if($qa==0){
     echo "您是第:<a style=';font-size:30px;color:yellow;'><i><b>";
     echo $n."</b></i></a>位提交者";
     $file=fopen("3.txt","a");
-    fwrite($file,$n." ".$_SERVER['REMOTE_ADDR']."\n");
+    fwrite($file,$n." ".$ip."\n");
     fclose($file);
     $n++;
     $file=fopen("1.txt","w");
