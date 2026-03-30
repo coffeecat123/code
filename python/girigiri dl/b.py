05442356b49fb9fa216c51eee38ca5bb87fae75b
coffeecat <coffeeccaatt@gmail.com>
2026-03-30 20:26:47 +0800

    Update download list structure and add '-extension_picky 0' option to ffprobe and ffmpeg commands for improved compatibility

--- python/girigiri dl/b.py
+++ python/girigiri dl/b.py
@@ -112,6 +112,7 @@ def get_duration(m3u8_url):
     try:
         command = [
             "ffprobe", "-v", "error", "-i", m3u8_url,
+            "-extension_picky", "0",
             "-show_entries", "format=duration", 
             "-of", "default=noprint_wrappers=1:nokey=1"
         ]
@@ -286,6 +287,7 @@ def download_episode(item_type, item, line_num):
     try:
         command = [
             "ffmpeg", "-v", "error", "-hide_banner", "-stats", 
+            "-extension_picky", "0",
             "-protocol_whitelist", "file,http,https,tcp,tls", 
             "-i", m3u8_url, "-c", "copy", "-y", output_file
         ]
